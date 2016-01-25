6b1d46b8949cf57d23d7c8bc738e827fbcdb14c2
iver56 <iver56@hotmail.com>
2016-01-25 10:57:17 +0100

    [WIP] Read analysis and use it as parameter for an effect

---
+++ cross_adapt.py
@@ -0,0 +1,55 @@
+import argparse
+import time
+import template_handler
+import csound_handler
+
+
+class Main(object):
+    def __init__(self):
+        arg_parser = argparse.ArgumentParser()
+        arg_parser.add_argument(
+            '-s',
+            '--sound',
+            dest='sound_filename',
+            type=str,
+            help='The name of the sound file to be processed',
+            required=True
+        )
+        arg_parser.add_argument(
+            '-d',
+            '--data',
+            dest='data_filename',
+            type=str,
+            help='The name of the data (json) file to be used as basis for input to the effect',
+            required=True
+        )
+        arg_parser.add_argument(
+            '--print-execution-time',
+            nargs='?',
+            dest='print_execution_time',
+            help='At the end of the run, print the execution time',
+            const=True,
+            required=False,
+            default=False
+        )
+        self.args = arg_parser.parse_args()
+
+        if self.args.print_execution_time:
+            self.start_time = time.time()
+
+        self.run()
+
+        if self.args.print_execution_time:
+            print "execution time: %s seconds" % (time.time() - self.start_time)
+
+    def run(self):
+        template = template_handler.TemplateHandler('templates/cross_adapt.csd.jinja2')
+        template.compile(sound_filename=self.args.sound_filename, data_filename=self.args.data_filename, krate=441)
+        csd_path = 'csd/cross_adapt.csd'
+        template.write_result(csd_path)
+        csound = csound_handler.CsoundHandler(csd_path)
+        output_filename = self.args.sound_filename + '.processed.wav'
+        csound.run(output_filename)
+
+if __name__ == '__main__':
+    Main()
---
+++ reader.py
@@ -0,0 +1,20 @@
+import json
+import os
+
+
+class Reader(object):
+    def __init__(self, data_filename):
+        with open(os.path.join('feature_data', data_filename)) as data_file:
+            self.data = json.load(data_file)
+        self.current_k = {}
+        for feature in self.data['series']:
+            self.current_k[feature] = 0
+
+    def get_next_value(self, feature):
+        if self.current_k[feature] < len(self.data['series'][feature]):
+            result = self.data['series'][feature][self.current_k[feature]]
+            self.current_k[feature] += 1
+            return result
+        print 'out of range', self.current_k[feature]
+        print 'feature', feature
+        return 0.0
