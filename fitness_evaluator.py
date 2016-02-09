7338988c9b6c1e3b7bf6cae75a8feaa014ef76ac
iver56 <iver56@hotmail.com>
2016-02-09 23:14:35 +0100

    Allow one to run fitness evaluator from the command line

--- fitness_evaluator.py
+++ fitness_evaluator.py
@@ -19,3 +19,30 @@ class FitnessEvaluator(object):
         mean_amp_difference = abs(mean_amp_c - mean_amp_a)
 
         return 1.0 / (1 + mean_amp_difference)
+
+
+class EvaluateFitness(object):
+    def __init__(self):
+        import argparse
+        import sound_file
+        arg_parser = argparse.ArgumentParser()
+        arg_parser.add_argument(
+            '-i',
+            '--input',
+            dest='input_files',
+            nargs='+',
+            type=str,
+            help='The names of the two sound files to be compared',
+            required=True,
+            default=[]
+        )
+        args = arg_parser.parse_args()
+        if len(args.input_files) == 2:
+            sound_file_a = sound_file.SoundFile(args.input_files[0])
+            sound_file_b = sound_file.SoundFile(args.input_files[1])
+            print(FitnessEvaluator.evaluate(sound_file_a, sound_file_b))
+        else:
+            raise Exception('Two file names must be specified')
+
+if __name__ == '__main__':
+    EvaluateFitness()
