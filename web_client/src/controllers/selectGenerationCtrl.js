0835a225d8a41e4dfaaec23d53b403d63e27c9ef
iver56 <iver56@hotmail.com>
2016-03-01 14:39:34 +0100

    Remove custom arrow keys navigation, as angular-material also implements that

--- web_client/src/controllers/selectGenerationCtrl.js
+++ web_client/src/controllers/selectGenerationCtrl.js
@@ -4,30 +4,4 @@ angular.module('crossAdaptiveAudioApp')
   .controller('SelectGenerationCtrl', function($scope, statsService) {
     var vm = this;
     vm.statsService = statsService;
-
-    vm.onKeyUp = function (e) {
-      var noOp = false;
-      if (e.keyCode === 37) { // left arrow
-        if (statsService.selectedGeneration > 1) {
-          statsService.selectedGeneration -= 1;
-        }
-      } else if (e.keyCode === 39) { // right arrow
-        if (statsService.selectedGeneration < statsService.numGenerations) {
-          statsService.selectedGeneration += 1;
-        }
-      } else {
-        // no operation
-        noOp = true;
-      }
-      if (!noOp) {
-        if (!$scope.$$phase) {
-          $scope.$apply();
-        }
-      }
-    };
-
-    vm.init = function() {
-      window.addEventListener("keyup", vm.onKeyUp, false);
-    };
-    vm.init();
   });
