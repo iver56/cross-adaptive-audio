91fa4764eab3835f42779d47d7c177c685d10c48
iver56 <iver56@hotmail.com>
2016-03-02 22:21:57 +0100

    Implement basic neural network visualization

---
+++ web_client/src/directives/neuralNetworkViz.js
@@ -0,0 +1,36 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('crossAdaptiveAudioApp')
+    .directive('neuralNetworkViz', NeuralNetworkViz);
+
+  function NeuralNetworkViz() {
+
+    function NeuralNetworkVizCtrl($scope) {
+      var vm = this;
+
+      vm.sigmaContainer = document.getElementById('sigma-container');
+      vm.sigmaInstance = new window.sigma(vm.sigmaContainer);
+
+      $scope.$watch(function() {
+        return vm.graph;
+      }, function() {
+        vm.sigmaInstance.graph.clear();
+        vm.sigmaInstance.graph.read(vm.graph);
+        vm.sigmaInstance.refresh();
+      })
+    }
+
+    return {
+      restrict: 'E',
+      scope: {},
+      bindToController: {
+        graph: '='
+      },
+      templateUrl: 'views/neural-network-viz.html',
+      controller: NeuralNetworkVizCtrl,
+      controllerAs: 'nnv'
+    };
+  }
+})();
