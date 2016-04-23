74041b59648a25f8c85cd0d3a2be554ae864d475
iver56 <iver56@hotmail.com>
2016-04-23 17:49:51 +0200

    Prettier print @ serve

--- serve.py
+++ serve.py
@@ -13,7 +13,7 @@ else:
     raise Exception('NodeJS is not installed. Please install it.')
 
 command.append(os.path.join(settings.BASE_DIR, 'node_server', 'server.js'))
-print(str(command))
+print(' '.join(command))
 
 p = subprocess.Popen(command)
 p.wait()
